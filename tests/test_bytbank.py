69e3e32796f14d64d78a4ec88c609101f3f895bf
evertondutra <evertondutra02@gmail.com>
2022-09-17 20:53:29 -0300

    teste da função sobrenome

--- tests/test_bytbank.py
+++ tests/test_bytbank.py
@@ -1,18 +1,25 @@
 from codigo.bytebank import Funcionario
 
 
-
 class TestClass:
+
     def test_quando_idade_recebe_13_03_2000_deve_retornar_(self):
         entrada = '13/03/2000'
         esperado = 22
-
         # Given
         funcionario_teste = Funcionario('Teste', entrada, 1111)
-
         # When
         resultado = funcionario_teste.idade()
-
-
         # Then
         assert resultado == esperado
+
+    def test_quando_insere_nome_completo_retorna_ultimo_sobrenome(self):
+        entrada = 'Eduardo Dutra'
+        esperado = 'Dutra'
+
+        funcionario = Funcionario(entrada, '13/01/2000', 1111)
+
+        resultado = funcionario.sobrenome()
+
+        assert resultado == esperado
+
