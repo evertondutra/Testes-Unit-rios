e9c05ba9da7c43c5b1569dc2aa13f6d71d41a2d0
evertondutra <evertondutra02@gmail.com>
2022-09-17 22:17:12 -0300

    atualizando o requirements

--- tests/test_bytbank.py
+++ tests/test_bytbank.py
@@ -4,7 +4,6 @@ from codigo.bytebank import Funcionario
 
 class TestClass:
 
-
     def test_quando_idade_recebe_13_03_2000_deve_retornar_(self):
         entrada = '13/03/2000'
         esperado = 22
